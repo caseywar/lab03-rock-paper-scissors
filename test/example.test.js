595f3f89e056dbefa0e99c4967dbc8c67c826ef0
Casey Warren <caseywar1017@gmail.com>
2021-01-13 17:31:13 -0500

    tests added and makeRockPaperSissors function

--- test/example.test.js
+++ test/example.test.js
@@ -1,16 +1,44 @@
 // IMPORT MODULES under test here:
-// import { example } from '../example.js';
+import { makeRockPaperSissors } from '../utils.js';
 
 const test = QUnit.test;
 
-test('time to test a function', (expect) => {
+test('it should return rock when given a 1', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = true;
-    
+    const expected = 'rock';
+
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = false;
+    const actual = makeRockPaperSissors(1);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+test('it should return paper when given a 2', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = 'paper';
+
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = makeRockPaperSissors(2);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+test('it should return sissors when given a 3', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = 'sissors';
+
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = makeRockPaperSissors(3);
 
     //Expect
     // Make assertions about what is expected versus the actual result
---
+++ utils.js
@@ -0,0 +1,11 @@
+export function makeRockPaperSissors(throwOptions) {
+    if (throwOptions === 1) {
+        return 'rock';
+    }
+    if (throwOptions === 2) {
+        return 'paper';
+    }
+    if (throwOptions === 3) {
+        return 'sissors'
+    }
+}
