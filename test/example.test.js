9a83f0ceb40f9928d47235d7b10beb972e88934f
Patrick Hrabos <phrabos@gmail.com>
2021-01-14 17:34:01 -0500

    app.js refactoring and tests

--- test/example.test.js
+++ test/example.test.js
@@ -1,5 +1,5 @@
 // IMPORT MODULES under test here:
-import { getRandomThrow, didUserWin } from '../utils.js';
+import { getRandomThrow, didUserWin } from '../get-random-throw.js';
 
 const test = QUnit.test;
 
@@ -10,7 +10,7 @@ test('it should return rock when given a 1', (expect) => {
 
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = getRandomThrow(1);
+    const actual = getRandomThrow();
 
     //Expect
     // Make assertions about what is expected versus the actual result
@@ -27,4 +27,14 @@ test('test should determine that player won when player plays rock and computer 
     //Expect
     // Make assertions about what is expected versus the actual result
     expect.equal('win', result);
+});
+test('test should determine that player won when player plays rock and computer plays sissors', (expect) => {
+    const player = 'rock';
+    const computer = 'rock';
+
+    const result = didUserWin(player, computer);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal('draw', result);
 });
